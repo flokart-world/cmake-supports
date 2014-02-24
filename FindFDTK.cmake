b4b951f664db1d8deef1ac96567a1fd62aa7692b
Shintaro Sakahara <sakahara.shintaro@bpsinc.jp>
2014-02-24 20:01:55 +0900

    Revise the directory as module path.

---
+++ FindFDTK.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2014 BPS Co., Ltd.
+# All rights reserved.
+
+find_package(PkgConfig REQUIRED)
+PKG_CHECK_MODULES(FDTK REQUIRED fdtk_for_bps)
---
+++ FindLibHyperBook.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2014 BPS Co., Ltd.
+# All rights reserved.
+
+find_package(PkgConfig REQUIRED)
+PKG_CHECK_MODULES(LIBHYPERBOOK REQUIRED libhyperbook)
