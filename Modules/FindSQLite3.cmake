fa3110d2685b73cc8216b71be8bab424df181596
Shintaro Sakahara <shintaro-sakahara@flokart-world.co.jp>
2014-03-28 04:09:08 +0900

    Add module scripts for libsquish and SQLite3.

---
+++ Modules/FindSQLite3.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2014 Flokart World, Inc.
+#
+# This software is provided 'as-is', without any express or implied
+# warranty. In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# 
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 
+#    1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 
+#    2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 
+#    3. This notice may not be removed or altered from any source
+#    distribution.
+
+CMS_FIND_PACKAGE(SQLITE3 sqlite3)
+set (SQLITE3_LIBRARIES "${SQLITE3_LIBRARY_DIR}/sqlite3")
+
+include (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLite3
+                                  REQUIRED_VARS
+                                  SQLITE3_INCLUDE_DIR
+                                  SQLITE3_LIBRARY_DIR
+                                  SQLITE3_LIBRARIES
+                                  VERSION_VAR
+                                  SQLITE3_VERSION_STRING)
