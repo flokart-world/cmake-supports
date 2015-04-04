flokart-world/cmake-supports

commit 35b14135f38976719d685f4121e6ade2fbc812b6

Fix issue where imported modules don't work fine.

Private/Package.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Flokart World, Inc.
+# Copyright (c) 2014-2015 Flokart World, Inc.
 #
 # This software is provided 'as-is', without any express or implied
 # warranty. In no event will the authors be held liable for any damages
@@ -29,15 +29,11 @@ function (CMS_QUALIFY_VARIABLE _ret _name)
 endfunction ()
 
 function (CMS_REGISTER_PACKAGE _name)
-  if (ARGN)
-    list (GET ARGN 0 _prefix)
-  else ()
-    set (_prefix "${_name}")
-  endif ()
+  CMS_ASSERT_IDENTIFIER(${_name})
 
   CMS_QUALIFY_PACKAGE_PREFIX(_qname ${_name})
   CMS_ENSURE_CMAKE_PROPERTY(GLOBAL PROPERTY ${_qname})
-  set_property (GLOBAL PROPERTY "${_qname}" "${_prefix}")
+  set_property (GLOBAL PROPERTY ${_qname} ${_name})
 endfunction ()
 
 function (CMS_GET_PACKAGE_PREFIX _ret _name)
@@ -76,6 +72,7 @@ endfunction ()
 # This function may be called more than once for a package within a same
 # scope, due to set of required components.
 function (CMS_DEFINE_PACKAGE_INTERFACE _package _prefix)
+  CMS_ASSERT_IDENTIFIER(${_package})
   CMS_PACKAGE_INTERFACE(_target ${_package})
 
   if (${_prefix}_LIBRARY_DIRS)
@@ -127,20 +124,24 @@ endfunction ()
 function (CMS_TEST_PACKAGE _ret _name)
   CMS_PACKAGE_INTERFACE(_target ${_name})
 
-  if (TARGET "${_target}")
+  if (TARGET ${_target})
     CMS_PACKAGE_COMPONENTS(_qname ${_target})
     get_cmake_property (_loadedComponents ${_qname})
 
-    if (_loadedComponents)
-      list (REMOVE_ITEM ARGN ${_loadedComponents})
-    endif ()
+    if (ARGN)
+      if (_loadedComponents)
+        list (REMOVE_ITEM ARGN ${_loadedComponents})
+      endif ()
 
-    list (LENGTH ARGN _length)
+      list (LENGTH ARGN _length)
 
-    if (_length EQUAL 0)
-      CMS_RETURN(_ret true)
+      if (_length EQUAL 0)
+        CMS_RETURN(_ret true)
+      else ()
+        CMS_RETURN(_ret false)
+      endif ()
     else ()
-      CMS_RETURN(_ret false)
+      CMS_RETURN(_ret true)
     endif ()
   else ()
     CMS_RETURN(_ret false)
@@ -201,7 +202,7 @@ function (CMS_LOAD_PACKAGE _name)
     find_package ("${_name}" ${ARGN})
 
     if (${_prefix}_FOUND)
-      CMS_REGISTER_PACKAGE(${_name} ${_prefix})
+      CMS_REGISTER_PACKAGE(${_name})
       CMS_DEFINE_PACKAGE_INTERFACE(${_name} ${_prefix} ${_components})
     endif ()
   endif ()
@@ -212,9 +213,18 @@ function (CMS_PROVIDE_PACKAGE _name)
   CMS_TEST_PACKAGE(_loaded ${_name})
 
   if (NOT _loaded)
-    CMS_REGISTER_PACKAGE(${_name} ${_prefix})
-    CMS_DEFINE_PACKAGE_INTERFACE(${_name} ${_prefix} ${ARGN})
+    CMS_REGISTER_PACKAGE(${_name})
   endif ()
+
+  CMS_PACKAGE_INTERFACE(_target ${_name})
+
+  if (NOT TARGET ${_target})
+    # A provided package corresponds to an embedded(virtual) package.
+    # It prevents non-global library from being added.
+    add_library (${_target} INTERFACE IMPORTED GLOBAL)
+  endif ()
+
+  CMS_DEFINE_PACKAGE_INTERFACE(${_name} ${_name} ${ARGN})
 endfunction ()
 
 function (CMS_TEST_VARIABLE _ret _name)

Private/Scope.cmake
@@ -288,31 +288,43 @@ function (CMS_EXPAND_VARIABLES _ret)
 endfunction ()
 
 function (CMS_INCLUDE_DIRECTORIES)
-  if (ARGN)
-    CMS_EXPAND_VARIABLES(_expanded ${ARGN})
-    CMS_DEFAULT_SCOPED_PROPERTY(_values ${_expanded})
-    CMS_APPEND_TO_PROPERTY(IncludeDirectories ${_values})
-  else ()
-    message (FATAL_ERROR "At least one argument must be specified.")
+  get_directory_property (_this CMS::Scope::This)
+
+  if (_this)
+    if (ARGN)
+      CMS_EXPAND_VARIABLES(_expanded ${ARGN})
+      CMS_DEFAULT_SCOPED_PROPERTY(_values ${_expanded})
+      CMS_APPEND_TO_PROPERTY(IncludeDirectories ${_values})
+    else ()
+      message (FATAL_ERROR "At least one argument must be specified.")
+    endif ()
   endif ()
 endfunction ()
 
 function (CMS_LINK_DIRECTORIES)
-  if (ARGN)
-    CMS_EXPAND_VARIABLES(_expanded ${ARGN})
-    CMS_APPEND_TO_PROPERTY(LinkDirectories "${_expanded}")
-  else ()
-    message (FATAL_ERROR "At least one argument must be specified.")
+  get_directory_property (_this CMS::Scope::This)
+
+  if (_this)
+    if (ARGN)
+      CMS_EXPAND_VARIABLES(_expanded ${ARGN})
+      CMS_APPEND_TO_PROPERTY(LinkDirectories "${_expanded}")
+    else ()
+      message (FATAL_ERROR "At least one argument must be specified.")
+    endif ()
   endif ()
 endfunction ()
 
 function (CMS_LINK_LIBRARIES)
-  if (ARGN)
-    CMS_EXPAND_VARIABLES(_expanded ${ARGN})
-    CMS_DEFAULT_SCOPED_PROPERTY(_values ${_expanded})
-    CMS_APPEND_TO_PROPERTY(LinkLibraries ${_values})
-  else ()
-    message (FATAL_ERROR "At least one argument must be specified.")
+  get_directory_property (_this CMS::Scope::This)
+
+  if (_this)
+    if (ARGN)
+      CMS_EXPAND_VARIABLES(_expanded ${ARGN})
+      CMS_DEFAULT_SCOPED_PROPERTY(_values ${_expanded})
+      CMS_APPEND_TO_PROPERTY(LinkLibraries ${_values})
+    else ()
+      message (FATAL_ERROR "At least one argument must be specified.")
+    endif ()
   endif ()
 endfunction ()
 
@@ -489,51 +501,59 @@ function (CMS_PROPAGATE_PROPERTY _key)
   CMS_ADD_TO_PARENT_PROPERTY("${_key}" ${_values})
 endfunction ()
 
-function (CMS_SET_VARIABLE _name _value)
+function (CMS_SET_VARIABLE _name)
   CMS_ASSERT_IDENTIFIER(${_name})
-  CMS_ASSERT_IDENTIFIER(${_value})
+  get_directory_property (_this CMS::Scope::This)
 
-  set (_qname Variable[${_name}])
-  CMS_PROPERTY_DEFINED(_defined ${_qname})
+  if (_this)
+    set (_qname Variable[${_name}])
+    CMS_PROPERTY_DEFINED(_defined ${_qname})
 
-  if (NOT _defined)
-    CMS_DEFINE_PROPERTY(${_qname})
+    if (NOT _defined)
+      CMS_DEFINE_PROPERTY(${_qname})
+    endif ()
+
+    CMS_SET_PROPERTY(${_qname} ${ARGN})
   endif ()
-
-  CMS_SET_PROPERTY(${_qname} ${_value})
 endfunction ()
 
 function (CMS_GET_VARIABLE _ret _name)
   CMS_ASSERT_IDENTIFIER(${_name})
-  set (_from "")
-  set (_qname Variable[${_name}])
+  get_directory_property (_this CMS::Scope::This)
 
-  while (ARGN)
-    list (GET ARGN 0 _arg)
-    list (REMOVE_AT ARGN 0)
+  if (_this)
+    set (_from "")
+    set (_qname Variable[${_name}])
 
-    if (_arg STREQUAL "FROM")
-      list (LENGTH ARGN _size)
+    while (ARGN)
+      list (GET ARGN 0 _arg)
+      list (REMOVE_AT ARGN 0)
 
-      if (_size GREATER 0)
-        list (GET ARGN 0 _from)
-        list (REMOVE_AT ARGN 0)
+      if (_arg STREQUAL "FROM")
+        list (LENGTH ARGN _size)
+
+        if (_size GREATER 0)
+          list (GET ARGN 0 _from)
+          list (REMOVE_AT ARGN 0)
+        else ()
+          message (FATAL_ERROR "FROM option requires one argument.")
+        endif ()
       else ()
-        message (FATAL_ERROR "FROM option requires one argument.")
+        message (FATAL_ERROR "Unrecognized option: \"${_arg}\"")
       endif ()
+    endwhile ()
+
+    if (_from)
+      CMS_QUALIFY_NAMESPACE(_ns ${_from})
+      CMS_GET_QNAME_PROPERTY(_value ${_ns}::${_qname})
     else ()
-      message (FATAL_ERROR "Unrecognized option: \"${_arg}\"")
+      CMS_GET_PROPERTY(_value ${_qname})
     endif ()
-  endwhile ()
 
-  if (_from)
-    CMS_QUALIFY_NAMESPACE(_ns ${_from})
-    CMS_GET_QNAME_PROPERTY(_value ${_ns}::${_qname})
+    CMS_RETURN(_ret \${_value})
   else ()
-    CMS_GET_PROPERTY(_value ${_qname})
+    CMS_RETURN(_ret)
   endif ()
-
-  CMS_RETURN(_ret \${_value})
 endfunction ()
 
 function (CMS_IMPORT_TARGET_DEPENDENCIES)

commit 27dc97d8acafbffcd5e5f4c87d345dee56f80b66

Fix issue where generation fails.
The fixed problem is caused by the commit:
    35b14135f38976719d685f4121e6ade2fbc812b6

CMS_REGISTER_PACKAGE must take the second argument when it is invoked via
CMS_LOAD_PACKAGE with PREFIX option.

Private/Package.cmake
@@ -31,9 +31,15 @@ endfunction ()
 function (CMS_REGISTER_PACKAGE _name)
   CMS_ASSERT_IDENTIFIER(${_name})
 
+  if (ARGN)
+    list (GET ARGN 0 _prefix)
+  else ()
+    set (_prefix ${_name})
+  endif ()
+
   CMS_QUALIFY_PACKAGE_PREFIX(_qname ${_name})
   CMS_ENSURE_CMAKE_PROPERTY(GLOBAL PROPERTY ${_qname})
-  set_property (GLOBAL PROPERTY ${_qname} ${_name})
+  set_property (GLOBAL PROPERTY ${_qname} ${_prefix})
 endfunction ()
 
 function (CMS_GET_PACKAGE_PREFIX _ret _name)
@@ -202,7 +208,7 @@ function (CMS_LOAD_PACKAGE _name)
     find_package ("${_name}" ${ARGN})
 
     if (${_prefix}_FOUND)
-      CMS_REGISTER_PACKAGE(${_name})
+      CMS_REGISTER_PACKAGE(${_name} ${_prefix})
       CMS_DEFINE_PACKAGE_INTERFACE(${_name} ${_prefix} ${_components})
     endif ()
   endif ()