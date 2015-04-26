514847a72037f5f1d6f9b871e46f860ba46b5c2a
Shintaro Sakahara <shintaro-sakahara@flokart-world.co.jp>
2015-04-27 01:48:18 +0900

    Fix issue where Test block doesn't work.

--- Private/Scopes/Test.cmake
+++ Private/Scopes/Test.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Flokart World, Inc.
+# Copyright (c) 2014-2015 Flokart World, Inc.
 #
 # This software is provided 'as-is', without any express or implied
 # warranty. In no event will the authors be held liable for any damages
@@ -19,17 +19,17 @@
 #    3. This notice may not be removed or altered from any source distribution.
 
 if (CMS_SCOPE_CALL STREQUAL "INIT")
-  set (ENABLE_TESTING true CACHE BOOL "Set true to enable testing")
-
-  function (CMS_ENABLE_TESTING)
+  macro (CMS_ENABLE_TESTING)
     if (ENABLE_TESTING)
       enable_testing ()
     endif ()
-  endfunction ()
+  endmacro ()
 elseif (CMS_SCOPE_CALL STREQUAL "BEGIN")
+  set (ENABLE_TESTING true CACHE BOOL "Set true to enable testing")
+
   list (GET ARGN 0 _name)
 
-  CMS_DEFINE_TARGET("${_name}")
+  CMS_DEFINE_EXECUTABLE("${_name}")
 
   CMS_STACK_PUSH("${_name}")
 elseif (CMS_SCOPE_CALL STREQUAL "END")
